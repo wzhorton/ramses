27fd3351160a7c810c0a943681452c661fedb1f0
wzhorton <wz.horton@gmail.com>
2018-08-10 23:12:25 -0600

    Version 0.0.6, adds functional vignette and code to build it.

--- compile_ramses.R
+++ compile_ramses.R
@@ -1,6 +1,7 @@
 #### compile_ramses.R ####
 # This file is only meant to be used for quick recompilation during development.
 # For regular installation use devtools::install_github
+# Use devtools::install_github(build_vignettes = TRUE) to get html vignettes
 
 library(roxygen2)
 library(devtools)
@@ -16,4 +17,6 @@ file.remove("R/RcppExports.R")
 devtools::document("../ramses")
 
 # install preliminary package for function testing and documentation inspection
-devtools::install("../ramses")
+# setting build_vignettes = FALSE will considerably speed up installation.
+# if built with vignettes, use browseVignettes("ramses")
+devtools::install("../ramses", build_vignettes = FALSE)
